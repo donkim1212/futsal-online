cae988847d494d282efc6443f0371ec41a5107ef
Dongkyun Kim <donkim0122@gmail.com>
2024-05-31 15:45:36 +0900

    Update: userChecker, playerChecker Update: checkers now receive 'select' obj literal containing keys to be selected

--- src/lib/errors/error-checker.js
+++ src/lib/errors/error-checker.js
@@ -1,12 +1,40 @@
+import { prisma } from "";
+import PlayerNotFoundError from "./classes/player-not-found.error.js";
+import UserNotFoundError from "./classes/user-not-found.error.js";
+
+const queryBuilder = (where, select) => {
+  const query = {
+    where: { ...where },
+  };
+  if (select && Object.keys(select).length > 0) query.select = select;
+  return query;
+};
+
 export default errorChecker = {
   loginChecker: async function (username, password) {
     return {};
   },
   userChecker: async function (userId) {
-    return {};
+    const where = {
+      userId: userId,
+    };
+    const select = {
+      userId: true,
+      username: true,
+      rating: true,
+    };
+    const user = await prisma.user.findUnique(queryBuilder(where, select));
+    if (!user) throw new UserNotFoundError();
+    return user;
   },
   playerChecker: async function (playerId) {
-    return {};
+    const where = {
+      playerId: playerId,
+    };
+    const select = {};
+    const player = await prisma.player.findUnique(queryBuilder(where, select));
+    if (!player) throw new PlayerNotFoundError();
+    return player;
   },
   inventoryChecker: async function (userId, playerId) {
     return {};
