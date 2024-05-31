031309a356e606f635162be30feb7efaa88ea3f2
Dongkyun Kim <donkim0122@gmail.com>
2024-05-31 18:05:17 +0900

    Update: import prisma clients & change user and player checkers accordingly Feat: inventoryChecker, moneyChecker to be used in routers

--- src/lib/errors/error-checker.js
+++ src/lib/errors/error-checker.js
@@ -1,6 +1,7 @@
-import { prisma } from "";
+import { playerPrisma, userPrisma } from "../utils/prisma/index.js";
 import PlayerNotFoundError from "./classes/player-not-found.error.js";
 import UserNotFoundError from "./classes/user-not-found.error.js";
+import NotEnoughMoneyError from "./classes/not-enough-money.error.js";
 
 const queryBuilder = (where, select) => {
   const query = {
@@ -11,36 +12,36 @@ const queryBuilder = (where, select) => {
 };
 
 export default errorChecker = {
-  loginChecker: async function (username, password) {
-    return {};
-  },
-  userChecker: async function (userId) {
-    const where = {
-      userId: userId,
-    };
-    const select = {
-      userId: true,
-      username: true,
-      rating: true,
-    };
-    const user = await prisma.user.findUnique(queryBuilder(where, select));
+  userChecker: async function (userId, select) {
+    const query = queryBuilder({ userId: userId }, select);
+    const user = await userPrisma.user.findUnique(query);
     if (!user) throw new UserNotFoundError();
     return user;
   },
-  playerChecker: async function (playerId) {
-    const where = {
-      playerId: playerId,
-    };
-    const select = {};
-    const player = await prisma.player.findUnique(queryBuilder(where, select));
+  playerChecker: async function (playerId, select) {
+    const query = queryBuilder({ playerId: playerId }, select);
+    const player = await playerPrisma.player.findUnique(query);
     if (!player) throw new PlayerNotFoundError();
     return player;
   },
-  inventoryChecker: async function (userId, playerId) {
-    return {};
+  inventoryChecker: async function (userId, playerId, select) {
+    // TODO: change query method, maybe checker function parameters as well
+    // -> inventoryId as parameter?
+    await userChecker(userId);
+    await playerChecker(playerId);
+    const query = queryBuilder({ userId: userId, playerId: playerId }, select);
+    const inventory = await userPrisma.inventory.findUnique(query);
+    if (!inventory)
+      throw new PlayerNotFoundError("선수를 보유하고 있지 않습니다.");
+    return inventory;
   },
-  moneyChecker: async function (userId, chargeMoney) {
-    return {};
+  moneyChecker: async function (userId, requiredMoney, select) {
+    if (!select) select = { money: true };
+    else if (!select.money) select.money = true;
+    const query = queryBuilder({ userId: userId }, select);
+    const user = await userPrisma.user.findUnique(query);
+    if (requiredMoney > user.money) throw new NotEnoughMoneyError();
+    return user;
   },
   pageChecker: async function (pageNumber, loadCount) {
     return [];
