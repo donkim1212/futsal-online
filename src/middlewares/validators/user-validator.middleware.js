24177a600421c6f780f31e1050983948d091db92
Dongkyun Kim <donkim0122@gmail.com>
2024-05-31 23:16:50 +0900

    Feat: add userId, isAll validation methods

--- src/middlewares/validators/user-validator.middleware.js
+++ src/middlewares/validators/user-validator.middleware.js
@@ -1,8 +1,14 @@
 import Joi from "joi";
 
-const userId = Joi.number().integer().min(1);
+const userId = Joi.number().strict().integer().min(1);
 const username = Joi.string().alphanum.min(6).max(20);
 const password = Joi.string().min(6).max(20);
+const money = Joi.number()
+  .strict()
+  .integer()
+  .min(0)
+  .max(Number.MAX_SAFE_INTEGER);
+const isAll = Joi.boolean().strict();
 
 const signInSchema = Joi.object({
   username: username.required(),
@@ -13,6 +19,14 @@ const signUpSchema = Joi.object({
   ...signInSchema,
   passwordConfirmation: Joi.string().valid(Joi.ref("password")).required(),
 });
+
+const userIdSchema = Joi.object({
+  userId: userId.required(),
+}).unknown(true);
+
+const isAllSchema = Joi.object({
+  isAll: isAll.required(),
+}).unknown(true);
 
 const userValidationErrorHandler = async (err, res, msg, code) => {
   return res
@@ -33,6 +47,24 @@ export default userValidatorJoi = {
   signUpValidation: async function (req, res, next) {
     try {
       await signUpSchema.validateAsync(req.body);
+      next();
+    } catch (err) {
+      return userValidationErrorHandler(err, res);
+    }
+  },
+
+  userIdValidation: async function (req, res, next) {
+    try {
+      await userIdSchema.validateAsync(req.params);
+      next();
+    } catch (err) {
+      return userValidationErrorHandler(err, res);
+    }
+  },
+
+  isAllValidation: async function (req, res, next) {
+    try {
+      await isAllSchema.validateAsync(req.body);
       next();
     } catch (err) {
       return userValidationErrorHandler(err, res);
