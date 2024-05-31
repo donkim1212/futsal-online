1b3159e5c63e1615394a98b2c2fd51b929b74f8c
Dongkyun Kim <donkim0122@gmail.com>
2024-05-31 16:11:46 +0900

    Rename: move utils folder into lib

--- src/lib/utils/prisma/index.js
+++ src/lib/utils/prisma/index.js
@@ -1,7 +1,7 @@
 // src/utils/prisma/index.js
 
-import { PrismaClient as UserPrismaClient } from "../../../prisma/user/index.js";
-import { PrismaClient as playerPrismaPrismaClient } from "../../../prisma/player/index.js";
+import { PrismaClient as UserPrismaClient } from "../../../../prisma/user/index.js";
+import { PrismaClient as playerPrismaPrismaClient } from "../../../../prisma/player/index.js";
 
 export const playerPrisma = new playerPrismaPrismaClient({
   log: ["query", "info", "warn", "error"],
