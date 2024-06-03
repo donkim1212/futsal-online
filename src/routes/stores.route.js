2cdd13dad7908095942a60f80bfaf04595952659
Dongkyun Kim <donkim0122@gmail.com>
2024-06-03 16:38:34 +0900

    Feat: cash purchase feature

--- src/routes/stores.route.js
+++ src/routes/stores.route.js
@@ -1,11 +1,31 @@
 import express from "express";
 import ua from "../middlewares/auths/user.authenticator.js";
+import { userPrisma } from "../lib/utils/prisma/index.js";
 
 const router = express.Router();
 
 router.post("/stores/gacha", ua.authStrict, async (req, res, next) => {
   try {
     //
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.patch("/stores/cash/buy", ua.authStrict, async (req, res, next) => {
+  try {
+    //
+    const { amount } = req.body;
+    await userPrisma.user.update({
+      where: { userId: req.body.user.userId },
+      data: {
+        money: {
+          increment: amount,
+        },
+      },
+    });
+
+    return res.status(200).json({ message: `${amount}원 충전 완료.` });
   } catch (err) {
     next(err);
   }
