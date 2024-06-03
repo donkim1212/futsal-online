a9f82b574893e271c6c50e2239cc95b2e6a01379
Dongkyun Kim <donkim0122@gmail.com>
2024-06-03 13:59:30 +0900

    Update: split game's play method into two; versus and matchmaking

--- src/routes/games.route.js
+++ src/routes/games.route.js
@@ -6,7 +6,19 @@ const router = express.Router();
 
 const matchMaking = () => {};
 
-router.post("/games/play", ua.authStrict, async (req, res, next) => {
+router.post("/games/play/:userId", ua.authStrict, async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const opponent = await ec.playerChecker(userId);
+    const myTeam = await ec.teamChecker(req.body.user.userId);
+    const opTeam = await ec.teamChecker(userId);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//
+router.post("/games/matchmaking", ua.authStrict, async (req, res, next) => {
   try {
     //
   } catch (err) {
