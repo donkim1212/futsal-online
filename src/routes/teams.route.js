1f8535fef76f2d3437945797491e1558fc1fae57
큐아 <QUAH@simgyuhyeon-ui-MacBookPro.local>
2024-06-05 17:55:54 +0900

    fixed deleting, adding, getting list APIs

--- src/routes/teams.route.js
+++ src/routes/teams.route.js
@@ -17,13 +17,13 @@ router.post(
       // Check if the player is in the user's inventory
       const myInventory = await errorChecker.inventoryUserChecker( userId,inventoryId);
       if (!myInventory){
-        return res.status(404).json({message:"해당 플레이어가 존재하지 않습니다"});
+        return res.status(404).json({message:"해당 플레이어를 보유하고 있지 않습니다"});
       }
 
       // Check if the player is already on the team
       const existingPlayer = await userPrisma.team.findFirst({
         where: {
-          InventoryId: +inventoryId,
+          InventoryId: inventoryId,
         },
       });
 
@@ -33,17 +33,23 @@ router.post(
           .json({ message: "이미 팀에 추가된 플레이어입니다." });
       }
 
+      const existingPlayerCount = await userPrisma.team.count({
+        where: {
+          UserId: userId,
+        },
+      });
+      
+      if (existingPlayerCount >= 3) {
+        return res.status(400).json({ message: "팀에 추가할 수 있는 플레이어 수는 최대 3명입니다." });
+      }
+
     // Add player to team
     const addPlayer = await userPrisma.team.create({
       data: {
-        User: { connect: { userId: +userId } }, // Connect to an existing user
-        Inventory: { connect: { inventoryId: +inventoryId } }, // Connect to an existing inventory
-      },
-      include: {
-        Inventory: true, 
+        UserId: userId, 
+        InventoryId : inventoryId,
       },
     });
-    const teamPlayerInventoryId = addPlayer.Inventory.inventoryId;
 
     //Reduce inventoryId count
     const transaction = await userPrisma.$transaction(async (prisma) => {
@@ -74,8 +80,6 @@ router.delete(
     const { inventoryId } = req.body;
 
     try {
-      // //Check if user exists
-      // await errorChecker.userChecker(userId);
 
       //Check if the player is in the user's team
       const equippedPlayer = await userPrisma.team.findFirst({
@@ -103,7 +107,9 @@ router.delete(
           inventoryId:+inventoryId,
         },
         data:{
-          count: 1,
+          count: {
+            increment:1,
+          },
         }
       })
 
@@ -118,22 +124,26 @@ router.delete(
 
 /** Team Players List Api **/
 router.get(
-  "/teams",
-  userAuthMiddleware.authOptional,
-  async (req, res, next) => {
-    const userId = req.body.user?.userId || null;
+  "/teams/:userId", async (req, res, next) => {
+    const {userId} = req.params;
 
-    const teamPlayers = await userPrisma.team.findMany({
-      where: {
-        UserId: +userId,
-      },
-      
-      orderBy: {
-        teamId: "asc",
-      },
-    });
-    return res.status(200).json({ data: teamPlayers });
-  },
+    try {
+      const teamPlayers = await userPrisma.team.findMany({
+        where: {
+          UserId: +userId, 
+        },
+        select: {
+          teamId: true,
+          UserId: true,
+          InventoryId: true,
+        },
+      });
+
+      return res.status(200).json({ data: teamPlayers });
+    } catch (error) {
+      return next(error);
+    }
+  }
 );
 
 export default router;
